a4caf591eda7da14aba48e8c1aedf13ce99012be
mohanaruban619 <ruban.madrid16@gmail.com>
2025-03-09 09:41:28 +0530

    correction

--- src/AllRoutes.jsx
+++ src/AllRoutes.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Routes,Route} from 'react-router-dom'
 import Home from './Pages/Home/Home'
-import ContactUs from './Pages/ContactUs/ContactUs'
+// import ContactUs from './Pages/ContactUs/ContactUs'
 
 const AllRoutes = () => {
   return (
